8b6a41bbd0342e22c2c569bf773319ee29175312
齐硕 <2691171990@qq.com>
2019-09-29 20:43:32 +0800

    update test

--- minimarket/src/test/java/MissionDaoTest.java
+++ minimarket/src/test/java/MissionDaoTest.java
@@ -21,37 +21,56 @@ import static com.minimarket.utils.HttpUtil.doPost;
 public class MissionDaoTest {
     @Autowired
     private userMissionDao dao;
+    @Autowired
     private MissionDao mdao;
+
     @Test
-    public void  testSelectMission() throws Exception {
+    public void testSelectMission() throws Exception {
         userMission userMission = new userMission();
         userMission.setID("12");
         String res = dao.selectReceiver(userMission);
         System.out.println(res);
     }
-    @Test
-    public void  httpSelectMissionTest() throws Exception {
-        //userMission userMission = new userMission();
-        String url="http://localhost:8080/minimarket/mission/selectMissionListAll";
-        JSONObject json=new JSONObject();
-        json.put("ID","12");
 
-        JSONObject jsonObject = doPost(url,json);
+    @Test
+    public void httpSelectMissionTest() throws Exception {
+        //userMission userMission = new userMission();
+        String url = "http://localhost:8080/minimarket/mission/selectMissionListAll";
+        JSONObject json = new JSONObject();
+        json.put("ID", "12");
+
+        JSONObject jsonObject = doPost(url, json);
         System.out.println(jsonObject.toString());
     }
+
     @Test
-    public void testSelectMissionListAll() throws Exception{
+    public void testSelectMissionListAll() throws Exception {
         Mission mission = new Mission();
         mission = mdao.selectMissionListAll();
         System.out.println(mission);
     }
 
     @Test
-    public void testSelectMissionListUpload() throws Exception{
+    public void testSelectMissionListUpload() throws Exception {
         Mission mission = new Mission();
         mission.setOwner("1");
         Mission mission_1 = mdao.selectMissionListUpload(mission);
         System.out.println(mission_1);
     }
 
+    @Test
+    public void testSelectMissionListGet() throws Exception {
+        Mission mission = new Mission();
+        mission.setID("2");
+        Mission mission_1 = mdao.selectMissionListGet(mission);
+        System.out.println(mission_1);
+    }
+
+    @Test
+    public void testSelectMissionInfo() throws Exception {
+        Mission mission = new Mission();
+        mission.setID("12");
+        Mission mission_1 = mdao.selectMissionInfo(mission);
+        System.out.println(mission_1);
+    }
 }
